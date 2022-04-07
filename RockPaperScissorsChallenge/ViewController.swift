e3f2424430e0ffd96435550a56551bae92fa92b7
Lin LaMonte <penelopelamonte@gmail.com>
2022-04-07 14:09:43 -0600

    started mvp, very confused, oh god, are the buttons supposed to show up, why are the not showing up, setting up images, yup

--- RockPaperScissorsChallenge/ViewController.swift
+++ RockPaperScissorsChallenge/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var aiImage: UIImageView!
+    
+    @IBOutlet weak var chosenFighterImage: UIImageView!
+    
+    @IBOutlet weak var chooseRockFighter: UIImageView!
+    
+    @IBOutlet weak var choosePaperFighter: UIImageView!
+    
+    @IBOutlet weak var chooseScissorsFighter: UIImageView!
+    
+    @IBOutlet weak var winnerOfCombatLabel: UILabel!
+    
+        
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        chooseRockFighter.image = UIImage(named:"rock")
+        choosePaperFighter.image = UIImage(named: "rpaper")
+        chooseScissorsFighter.image = UIImage(named:"rscissors")
+        
     }
 
 
+    @IBAction func chooseRockFighterButton(_ sender: Any)
+    {
+    }
+    
+    @IBAction func choosePaperFighterButton(_ sender: Any)
+    {
+    }
+    
+    @IBAction func chooseScissorsFighterButton(_ sender: Any) {
+    }
 }
 
